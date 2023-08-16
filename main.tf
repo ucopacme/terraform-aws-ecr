3bcfeeb0070ebc7ac700d23703951f3714c33597
George Holbert <gholbert@ucop.edu>
2023-08-16 09:58:21 -0700

    removed unused ecr role

--- main.tf
+++ main.tf
@@ -1,21 +1,3 @@
-data "aws_iam_policy_document" "assume_by_ecr" {
-  statement {
-    sid     = ""
-    effect  = "Allow"
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type        = "Service"
-      identifiers = ["codebuild.amazonaws.com"]
-    }
-  }
-}
-
-resource "aws_iam_role" "ecr" {
-  name               = "${var.name}-ECR-role"
-  assume_role_policy = data.aws_iam_policy_document.assume_by_ecr.json
-}
-
 resource "aws_ecr_repository" "this" {
   name                 = join("-", [var.name])
   image_tag_mutability = var.image_tag_mutability
