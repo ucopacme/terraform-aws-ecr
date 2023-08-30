73b12b82622541b51053b6269298dfb3e4367c4b
George Holbert <gholbert@ucop.edu>
2023-08-30 14:44:28 -0700

    added arn to outputs

--- output.tf
+++ output.tf
@@ -1,3 +1,7 @@
+output "arn" {
+  description = "Full ARN of the repository."
+  value       = aws_ecr_repository.this.arn
+}
 output "registry_id" {
   description = "The account ID of the registry holding the repository."
   value       = aws_ecr_repository.this.registry_id
