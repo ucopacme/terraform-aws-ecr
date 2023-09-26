288c7873b34573bd324fd668b9d1dbd92ab7dc2d
George Holbert <gholbert@ucop.edu>
2023-09-25 17:15:44 -0700

    added name to outputs

--- output.tf
+++ output.tf
@@ -2,6 +2,10 @@ output "arn" {
   description = "Full ARN of the repository."
   value       = aws_ecr_repository.this.arn
 }
+output "name" {
+  description = "Name of the repository."
+  value       = aws_ecr_repository.this.name
+}
 output "registry_id" {
   description = "The account ID of the registry holding the repository."
   value       = aws_ecr_repository.this.registry_id
